9b4ee976c3054699a6b91a7294ada623e6478c5c
Keeplaughingxusertagbot <107563739+Keeplaughingxusertagbot@users.noreply.github.com>
2022-07-10 11:47:49 +0530

    Update vars.py

--- YukkiMusic/plugins/sudo/vars.py
+++ YukkiMusic/plugins/sudo/vars.py
@@ -64,8 +64,8 @@ async def varsFunc(client, message):
         start = "No"
     else:
         start = f"[Image]({config.START_IMG_URL})"
-    s_c = f"[Channel](https://t.me/TechZBots)"
-    s_g = f"[Group](https://t.me/TechZBots_Support)"
+    s_c = f"[Channel](https://t.me/sukoon_kii_talash)"
+    s_g = f"[Group](https://t.me/billufeelings)"
     if not config.GIT_TOKEN:
         token = "No"
     else:
